9a6de12b6524b9c06b629618262430b467650aa0
thevioletmaniac <thevioletmaniac@gmail.com>
2022-10-15 20:27:40 +0600

    Adjusted things template

--- src/components/planner/Things/Things.tsx
+++ src/components/planner/Things/Things.tsx
@@ -1,6 +1,8 @@
+import { nanoid } from 'nanoid';
 import React from 'react';
 import { useMemo } from 'react';
 import { useStoreon } from 'storeon/react';
+
 import { IThing } from '../../../interfaces';
 import Hero from '../../common/Hero';
 import Thing from '../Thing/Thing';
@@ -15,25 +17,43 @@ const Things: React.FC<Props> = () => {
   const { things } = useStoreon('things');
 
   const renderThings = useMemo(() => {
-    return things.map((thing: IThing) => {
-      const { id } = thing;
+    const plateLimit = 5;
+    const items = things.length > plateLimit
+      ? things
+      : new Array(plateLimit - things.length)
+        .fill(null)
+        .concat(things)
+        .reverse();
 
-      return (
-        <li
-          key={id}
-          className='col col--33'>
-          <Thing thing={thing} />
-        </li>
-      )
-    })
+    return items.map((thing: IThing | null) => {
+      if (thing) {
+        return (
+          <li
+            key={thing.id}
+            className='col col--33'>
+            <Thing thing={thing} />
+          </li>
+        );
+      } else {
+        return (
+          <li
+            key={nanoid()}
+            className='col col--33'>
+            <Thing />
+          </li>
+        );
+      }
+    });
   }, [things]);
 
   return (
     <Hero>
       <div className='things'>
         <ul className='row'>
-          <li className='col col--33'>
-            <Thing />
+          <li
+            key='initial-item'
+            className='col col--33'>
+            <Thing isInitial />
           </li>
           {renderThings}
         </ul>
