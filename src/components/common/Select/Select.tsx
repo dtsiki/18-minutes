2a6990c8c5855f27ef612c958864a36f58cc2897
thevioletmaniac <thevioletmaniac@gmail.com>
2022-10-19 15:41:25 +0600

    Added select component

---
+++ src/components/common/Select/Select.tsx
@@ -0,0 +1,53 @@
+import React, { useMemo, useState } from 'react';
+
+import './style.scss';
+
+interface Props {
+  label: string;
+  name: string;
+  placeholder: string;
+  options: Array<any>;
+  selectedOption?: any;
+}
+
+const Select: React.FC<Props> = ({ label, name, placeholder, options, selectedOption }: Props) => {
+  const [localValue, setLocalValue] = useState<any>(selectedOption.id || null);
+
+  const changeValue = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+    setLocalValue(e.target.value)
+  }
+
+  const renderOptions = useMemo(() => {
+    return options.map((item) => {
+      return (
+        <option
+          key={item.id}
+          value={item.id}
+          className='select__option'>
+          {item.title}
+        </option>
+      );
+    });
+  }, [options, localValue]);
+
+  return (
+    <div className='select'>
+      <label
+        htmlFor={name}
+        className='heading-l4'>
+        {label}
+      </label>
+      <select
+        name={name}
+        id={name}
+        className='select__control'
+        onChange={changeValue}
+        value={localValue}>
+        <option value=''>{placeholder}</option>
+        {renderOptions}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
